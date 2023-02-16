5517c8b5638550bde5b6f156b58a8878ee60e700
dipuakhando2001 <121944271+dipuakhando2001@users.noreply.github.com>
2023-02-16 19:01:06 +0600

    Update setlink.js

--- setlink.js
+++ setlink.js
@@ -14,9 +14,9 @@ userInput.question("Enter your Offer Link Here = ", (value) => {
   console.log(value);
   fs.writeFile(
     "link.js",
-    `const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqa2RpMmQ3V3hETFUxdktkekZSTjRnSVh0Umprd3xBQ3Jtc0tuemJ3dlRUTlFpRVZzWUJUVV9aa210ajhFS3RDQ0pkMjB6OU1IeGsxSDRhbTlvSkZDZ3RkNzBnX1J4dGJZVzQxejduYTRaYUFMUVhmT2hqWnQxclBRbEZXcWZBWWcwQ29BamFSeXN5UnBsUDY5UHd1aw&q=${value}&html_redirect=1";`,
+    `const offerLink = "vnd.youtube://www.${value}&html_redirect=1";`,
     function (err) {
-      console.log("Link set seccussful★ Done And past (netlify deploy --prod)");
+      console.log("Link set seccussful★ Done");
 
       process.exit();
     }
